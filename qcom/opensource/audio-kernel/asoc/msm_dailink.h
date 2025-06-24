2aa44067a85a175cfee75899ebca4ad6c4e8e7e7
toufu <hamm157famm7423@gmail.com>
2025-06-24 13:25:12 +0900

    audio-kernel: Add sheng support
    Change-Id: I18a1d6d555eef46102c7f191fb6748f2ca0d3538

--- qcom/opensource/audio-kernel/asoc/msm_dailink.h
+++ qcom/opensource/audio-kernel/asoc/msm_dailink.h
@@ -309,8 +309,17 @@ SND_SOC_DAILINK_DEFS(sec_tdm_rx_0,
 #elif defined(CONFIG_TARGET_PRODUCT_VERMEER)
 SND_SOC_DAILINK_DEFS(sec_tdm_rx_0,
 	DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
-	DAILINK_COMP_ARRAY(COMP_CODEC("aw882xx_smartpa.2-0035", "aw882xx-aif-2-35"),
+	DAILINK_COMP_ARRAY(COMP_CODEC("aw882xx_smartpa.2-0035", "aw882xx-aif-2-35")),
 			   COMP_CODEC("aw882xx_smartpa.2-0034", "aw882xx-aif-2-34")),
+#elif defined(CONFIG_TARGET_PRODUCT_SHENG)
+SND_SOC_DAILINK_DEFS(sec_tdm_rx_0,
+	DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("cs35l43.3-0040", "cs35l43-pcm"),
+			   		   COMP_CODEC("cs35l43.3-0041", "cs35l43-pcm"),
+					   COMP_CODEC("cs35l43.3-0042", "cs35l43-pcm"),
+					   COMP_CODEC("cs35l43.2-0040", "cs35l43-pcm"),
+					   COMP_CODEC("cs35l43.2-0041", "cs35l43-pcm"),
+					   COMP_CODEC("cs35l43.2-0042", "cs35l43-pcm")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
 #else
 SND_SOC_DAILINK_DEFS(sec_tdm_rx_0,
