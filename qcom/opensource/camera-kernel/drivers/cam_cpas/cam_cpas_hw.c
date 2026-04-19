bdd3577c6112843a2d26fb21a6d60ecb55fb92a0
WilliamSado <williamsado1004@gmail.com>
2026-04-19 16:53:27 +0000

    camera-kernel: Use __GFP_NOFAIL for cpas_core allocation per AOSPA

--- qcom/opensource/camera-kernel/drivers/cam_cpas/cam_cpas_hw.c
+++ qcom/opensource/camera-kernel/drivers/cam_cpas/cam_cpas_hw.c
@@ -3548,7 +3548,7 @@ int cam_cpas_hw_probe(struct platform_device *pdev,
 		return -ENOMEM;
 	}
 
-	cpas_core = kzalloc(sizeof(struct cam_cpas), GFP_KERNEL);
+	cpas_core = kzalloc(sizeof(struct cam_cpas), GFP_KERNEL | __GFP_NOFAIL);
 	if (!cpas_core) {
 		kfree(cpas_hw);
 		kfree(cpas_hw_intf);
